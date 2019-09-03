c272d904e35aef295e99499c2d493d32217d437f
anstjaos <vv_eros@naver.com>
2019-09-04 00:50:04 +0900

    peer1 190909

---
+++ peer1/channel-artifacts/invoke.sh
@@ -0,0 +1,8 @@
+FABRIC_PATH=/opt/gopath/src/github.com/hyperledger/fabric/peer
+echo $FABRIC_PATH
+
+export CORE_PEER_LOCALMSPID="Org0MSP"
+export CORE_PEER_MSPCONFIGPATH=$FABRIC_PATH/crypto/peerOrganizations/org0.example.com/users/Admin@org0.example.com/msp
+export CORE_PEER_ADDRESS=peer1.org0.example.com:7051
+
+peer chaincode invoke -o orderer0.example.com:7050 -C ch1 -n testnetCC -c '{"Args":["invoke","a","b","50"]}'
---
+++ peer1/channel-artifacts/join-channel-peer1.sh
@@ -0,0 +1,7 @@
+FABRIC_PATH=/opt/gopath/src/github.com/hyperledger/fabric/peer
+echo $FABRIC_PATH
+
+export CORE_PEER_LOCALMSPID="Org0MSP"
+export CORE_PEER_MSPCONFIGPATH=$FABRIC_PATH/crypto/peerOrganizations/org0.example.com/users/Admin@org0.example.com/msp
+export CORE_PEER_ADDRESS=peer1.org0.example.com:7051
+peer channel join -b ch1.block
